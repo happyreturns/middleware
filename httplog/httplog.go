d3c8c35ec8bebdd0c037a32e0b9168fb424c6762
brian harrison <brian@idiomatic.org>
2016-02-24 21:58:55 -0800

    consolidated/refactored

---
+++ httplog/httplog.go
@@ -0,0 +1,42 @@
+package httplog
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"path"
+)
+
+var Logger = log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds)
+
+type ResponseWriter interface {
+	http.ResponseWriter
+	Status() int
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func NewResponseWriter(rw http.ResponseWriter) ResponseWriter {
+	return &responseWriter{rw, http.StatusOK}
+}
+
+func (rw *responseWriter) WriteHeader(s int) {
+	rw.status = s
+	rw.ResponseWriter.WriteHeader(s)
+}
+
+func (rw *responseWriter) Status() int {
+	return rw.status
+}
+
+func Handler(handler http.Handler) http.Handler {
+	app := path.Base(os.Args[0])
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rw := NewResponseWriter(w)
+		handler.ServeHTTP(rw, r)
+		Logger.Printf("%s %s %s %d", app, r.Method, r.URL.Path, rw.Status())
+	})
+}
