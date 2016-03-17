aba70bde4b37c5fc686e955f941ce9b9de5223ab
brian harrison <brian@happyreturns.com>
2016-03-17 14:44:57 -0700

    gzip

---
+++ gzip.go
@@ -0,0 +1,42 @@
+// per https://gist.github.com/bryfry/09a650eb8aac0fb76c24
+// and https://github.com/NYTimes/gziphandler/blob/master/gzip.go
+
+package middleware
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"strings"
+)
+
+// Gzip Compression
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func (w gzipResponseWriter) Flush() {
+	w.Writer.Flush()
+	if fw, ok := w.ResponseWriter.(http.Flusher); ok {
+		fw.Flush()
+	}
+}
+
+func Gzip(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			handler.ServeHTTP(w, r)
+			return
+		}
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
+		gzw := gzipResponseWriter{gz, w}
+		handler.ServeHTTP(gzw, r)
+	})
+}
