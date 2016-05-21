a69c9219256a75b7a7493d5cb0ef573657b5d978
brian harrison <brian@happyreturns.com>
2016-05-20 18:22:45 -0700

    too many slashes

--- forcehttps.go
+++ forcehttps.go
@@ -9,9 +9,11 @@ func ForceHTTPS(h http.Handler) http.Handler {
 		// normally the test is `!= "https"`, but we're permitting
 		// Heroku-free dev-mode.
 		if r.Header.Get("X-Forwarded-Proto") == "http" {
-			u := *r.URL
-			u.Scheme = "https"
-			http.Redirect(w, r, u.String(), http.StatusMovedPermanently)
+			u := "https:" + r.URL.Path
+			if r.URL.RawQuery != "" {
+				u = u + "?" + r.URL.RawQuery
+			}
+			http.Redirect(w, r, u, http.StatusMovedPermanently)
 		} else {
 			h.ServeHTTP(w, r)
 		}
